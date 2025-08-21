64b61c44cf7b0800d94c7e32024965f8e91071bc
RXY-89 <xochilpilli.castilloa@uanl.edu.mx>
2025-08-20 20:36:23 -0600

    Estos son archivos extra que me faltó agregar

---
+++ EJERCICIO 6/AuditoriaBasica.psm1
@@ -0,0 +1,32 @@
+﻿function Obtener-UsuariosInactivos {
+<#
+.SYNOPSIS
+Obtiene usuarios locales habilitados que nunca han iniciado sesión.
+.DESCRIPTION
+Esta funci´n busca cuentas locales hailitadas que no tienen fecha de último inicio de sesión.
+.EXAMPLE
+Obtener-UsuariosInactivos
+.NOTES
+Puede audar a detectar cuentas innecesarias o riesgosas en auditorías básicas.
+#>
+
+    Get-LocalUser|Where-Object { $_.Enabled -eq $true -and -not $_.LastLogon}
+}
+
+function Obtener-ServiciosExternos {
+<#
+.SYNOPSIS 
+Obtiene servicio en ejecución que no pertenecen explícitamente a Windows.
+
+.DESCRIPTION
+Filtra servicios activos cuyo nombre descriptivo no contiene el término "Windows".
+
+.EXAMPLE
+Obtener-ServiciosExternos
+
+.NOTES
+Útil para detectar software de terceros corriendo en segundo plano.
+#> 
+    Get-Service|Where-Object{ $_.Status -eq "Running" -and $_.DisplayName -notmatch "Windows"}
+}
+
---
+++ EJERCICIO 6/PRINCIPAL.ps1
@@ -0,0 +1,18 @@
+﻿Clear-Host
+Import-Module AuditoriaBasica
+Write-Host "Auditoría básica de usuarios y servicios"
+Write-Host "1. Mostra usuarios inactivos"
+Write-Host "2. Mostrar serivicios externos activos"
+$opcion= Read-Host "Selecciona una opción (1 o 2)"
+
+switch ($opcion) {
+    "1" {
+        $usuarios= Obtener-UsuariosInactivos
+        $usuarios|Format-Table Name, Enabled, LasLogon -AutoSize
+        $usuarios|Export.Csv -PAth "$env:USERPROFILE\Desktop\users_inac.csv" -NoTypeInformation
+        Write-Host "`n Reporte generado: serv_e.html"
+    }
+    default {
+        Write-Host "Opción no válida"
+    }
+}
